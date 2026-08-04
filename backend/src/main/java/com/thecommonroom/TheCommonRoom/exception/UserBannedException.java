ea160c311e74445340a4fc7b1398d1dbeb437eb2
delfi-fenoy <delfina-noel-fenoy-rivas@estudiante.mdp.utn.edu.ar>
2026-08-04 19:30:19 -0300

    Login con user baneado
    - Lógica
    - Excepción + manejo

---
+++ backend/src/main/java/com/thecommonroom/TheCommonRoom/exception/UserBannedException.java
@@ -0,0 +1,7 @@
+package com.thecommonroom.TheCommonRoom.exception;
+
+public class UserBannedException extends RuntimeException {
+    public UserBannedException(String message) {
+        super(message);
+    }
+}
